c984cb6ab68066bc90db9c10ff980b400cceb81e
okap76 <diskdusk+github@gmail.com>
2020-06-14 09:51:23 +0900

    create.blade.phpのフォームtextにplaceholderを追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,11 +8,11 @@
     <title>Document</title>
 </head>
 <body>
-<h1>Create Index</h1>
+<h1>Post times in Slack</h1>
 
 
 {!! Form::open(['action' => 'SlackController@store']) !!}
-{!! Form::text('content','コメントを入力してください', ['class' => 'form-input']) !!}
+{!! Form::text('content', null, ['class' => 'form-input', 'placeholder' => 'コメントを入力してください']) !!}
 {!! Form::submit('投稿する') !!}
 {!! Form::close() !!}
 
